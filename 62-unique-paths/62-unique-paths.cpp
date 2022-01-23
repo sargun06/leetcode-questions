0c0cb92525a881111ead39f4d08356fa5bd4b347
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-01-24 00:00:38 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (23.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,27 +1,25 @@
-//     int dfs(vector<vector<int>>& dp, int i, int j) {
-//         if(i >= size(dp)   || j >= size(dp[0]))   return 0;     // out of bounds - invalid
-//         if(i == size(dp)-1 && j == size(dp[0])-1) return 1;     // reached end - valid path
-//         if(dp[i][j]) return dp[i][j];                           // directly return if already calculated
-//         return dp[i][j] = dfs(dp, i+1, j) + dfs(dp, i, j+1);    // store the result in dp[i][j] and then return
-//     }
-// };
-
 class Solution {
 public:
-    
-    int paths (int i, int j, vector<vector<int>>& dp)
-    {
-        if(i==0 && j==0)
-            return 1;
-        if(i<0 || j<0)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        return dp[i][j]=paths(i,j-1,dp)+paths(i-1,j,dp);
-    }
         
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m,vector<int>(n,-1));
-        return paths(m-1,n-1,dp);
+        for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 && j==0)
+                    dp[i][j]=1;
+                else
+                {
+                    int left=0, up=0;
+                    if(i>0)
+                        up=dp[i-1][j];
+                    if(j>0)
+                        left=dp[i][j-1];
+                    dp[i][j]=left+up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
