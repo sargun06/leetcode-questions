d8b2174ad9d23111693fa44410be8f342d017b50
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-01-30 13:45:04 +0530

    Time: 103 ms (44.93%), Space: 13 MB (98.69%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();  
+        sort(nums.begin() , nums.end());  // sort the array to use the two pointers method
+        vector<vector<int>> ans;  
+		
+        for(int i = 0 ; i < n; i++)
+        {
+            for(int j = i + 1; j < n ; j++)
+            {
+                int target_new=target-nums[i]-nums[j];
+			    int front= j+1, back=n-1;
+                while(front<back)
+                {
+                    int sum=nums[front]+nums[back];
+                    if(sum<target_new)
+                        front++;
+                    else if(sum>target_new)
+                        back--;
+                    else
+                    {
+                        ans.push_back({nums[i],nums[j],nums[front],nums[back]});
+                        int f=nums[front];
+                        int b=nums[back];
+
+                        while(front<back && nums[front]==f)
+                            front++;
+
+                        while(front<back && nums[back]==b)
+                            back--;
+                    }
+                }
+            while(j + 1 < n && nums[j + 1] == nums[j]) ++j;
+            }
+        while (i + 1 < n && nums[i + 1] == nums[i]) ++i;
+        }
+		
+        return ans;
+    }
+};
