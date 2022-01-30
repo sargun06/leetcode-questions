fb9d91c4aaaf05ec982359e4b56b9ab75e0408e9
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-01-30 17:08:29 +0530

    Time: 14 ms (17.36%), Space: 8.7 MB (41.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -6,7 +6,7 @@ public:
         vector<int> dp=triangle[n];
         for(int i=n-1; i>=0; i--)
         {
-            for(int j=0; j<triangle[i].size(); j++)
+            for(int j=0; j<=i; j++)
             {
                 dp[j]=triangle[i][j]+ min(dp[j+1], dp[j]);
             }
