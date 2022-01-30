929c9fa1bf2d7288c2e3dbda41db625a1f5dac36
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-01-30 16:54:29 +0530

    Time: 8 ms (63.01%), Space: 8.7 MB (41.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
-    int cal(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp, int n)
-    {
-        if(i==n)
-            return triangle[i][j];
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        int d= triangle[i][j]+ cal(i+1,j,triangle,dp,n);
-        int dg= triangle[i][j]+ cal(i+1,j+1,triangle,dp,n);
-        return dp[i][j]=min(d,dg);
-    }
-    
+   
     int minimumTotal(vector<vector<int>>& triangle) {
         int n= triangle.size()-1;
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        return cal(0,0,triangle,dp,n);
+        vector<int> dp=triangle[n];
+        for(int i=n-1; i>=0; i--)
+        {
+            for(int j=0; j<triangle[i].size(); j++)
+            {
+                dp[j]=triangle[i][j]+ min(dp[j+1], dp[j]);
+            }
+        }
+        return dp[0];
     }
 };
