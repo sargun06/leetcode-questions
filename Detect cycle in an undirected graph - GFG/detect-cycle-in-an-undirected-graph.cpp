4f494fa201c456d9446a40a573a1720c039b2a0b
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-05-03 14:54:34 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -5,15 +5,31 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    bool cycle(vector<int> &vis, vector<int> adj[], int node, int parent)
+  
+  //USING BFS
+  
+    bool cycle(vector<int> &vis, vector<int> adj[], int node, int &V)
     {
-        vis[node]=1;
-        for(auto i:adj[node])
+        queue<int> q;
+        q.push(node);
+        
+        while(!q.empty())
         {
-            if(vis[i]!=1)
-                cycle(vis,adj,i,node);
-            else if(i!=parent)
-                return true;
+            int temp=q.front();
+            q.pop();
+            if(vis[temp]==1)
+               return true;
+            else
+            {
+                vis[temp]=1;
+                for(auto i:adj[temp])
+                {
+                    if(vis[i]!=1)
+                    {
+                        q.push(i);
+                    }
+                }
+            }
         }
         return false;
     }
@@ -23,11 +39,36 @@ class Solution {
         for(int j=1;j<V;j++)
         {
             if(vis[j]!=1)
-                if(cycle(vis,adj,j,-1))return 1;
+                if (cycle(vis,adj,j,V))return 1;
         }
         return 0;
     }
 };
+
+//  USING DFS
+//      bool cycle(vector<int> &vis, vector<int> adj[], int node, int parent)
+//     {
+//         vis[node]=1;
+//         for(auto i:adj[node])
+//         {
+//             if(vis[i]!=1)
+//                 cycle(vis,adj,i,node);
+//             else if(i!=parent)
+//                 return true;
+//         }
+//         return false;
+//     }
+//     // Function to detect cycle in an undirected graph.
+//     bool isCycle(int V, vector<int> adj[]) {
+//         vector<int> vis(V+1,0);
+//         for(int j=1;j<V;j++)
+//         {
+//             if(vis[j]!=1)
+//                 if(cycle(vis,adj,j,-1))return 1;
+//         }
+//         return 0;
+//     }
+// };
 
 // { Driver Code Starts.
 int main() {
