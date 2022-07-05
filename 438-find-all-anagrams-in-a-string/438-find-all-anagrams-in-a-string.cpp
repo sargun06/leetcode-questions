acb0fe6975c681abf55e3e10880044c0880a80fd
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-07-05 14:49:31 +0530

    Time: 39 ms (30.32%), Space: 8.6 MB (90.43%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,30 +1,38 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string s, string p) {
-        sort(p.begin(), p.end());     
-        vector<int> index;
-        int cur=-2;
-        if(s.length()<p.length())
-           return index; 
-        for(long long i=0; i<(s.length()-p.length()+1); i++)
-        {     
-            if (i > 0 and s[i-1]  == s[i + p.length() -1]) {
-                
-                if (cur == i-1) {
-                    index.push_back(i);
-                    cur = i;                  
-                } 
-                continue;
-            }
-                string temp= s.substr(i, (p.length()));
-                cout<<temp<<" ";
-                sort(temp.begin(), temp.end());
-                if(temp==p)
-                {
-                    index.push_back(i); 
-                    cur=i;
-                }
-        }
-        return index;
-    }
+    vector<int> findAnagrams(string txt, string pat) {
+        unordered_map<int,int> mp;
+	    for(int i=0; i<pat.size(); i++)
+	    {
+	        mp[pat[i]]++;
+	    }
+        vector<int> ans;
+	    int i=0, j=0, size=pat.size(), count=mp.size();
+	    while(j<txt.size())
+	    {
+	        if(mp.find(txt[j])!=mp.end())
+	        {
+	            mp[txt[j]]--;
+	            if(mp[txt[j]]==0)
+	            count--;
+	        }
+	        if(j-i+1<size)
+	        j++;
+	        else if(j-i+1==size)
+	        {
+	            if(count==0)
+	            ans.push_back(i);
+	            if(mp.find(txt[i])!=mp.end())
+	            {
+	                mp[txt[i]]++;
+	                if(mp[txt[i]]==1)
+	                count++;
+	            }
+	        i++; j++;
+	        }
+	    }
+	    return ans;
+	}
+
+    
 };
