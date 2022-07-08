6e359a5c2a8308e0063a4201d7586d4e81aa12d2
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-07-08 12:24:47 +0530

    Added solution - LeetHub

--- Find the Safe Position - GFG/find-the-safe-position.cpp
+++ Find the Safe Position - GFG/find-the-safe-position.cpp
@@ -8,22 +8,28 @@ using namespace std;
 class Solution {
   public:
   
-    void solve(vector<int> &v, int k, int idx)
-    {
-        if(v.size()==1)
-        return;
+    // void solve(vector<int> &v, int k, int idx)
+    // {
+    //     if(v.size()==1)
+    //     return;
         
-        idx=(idx+k)%v.size();
-        v.erase(v.begin()+idx);
-        solve(v,k,idx);
-    }
+    //     idx=(idx+k)%v.size();
+    //     v.erase(v.begin()+idx);
+    //     solve(v,k,idx);
+    // }
+    // int safePos(int n, int k) {
+    //     // code here
+    //     vector<int> v;
+    //     for(int i=0;i<n;i++)
+    //     v.push_back(i+1);
+    //     solve(v,k-1,0);
+    //     return v[0];
+    // }
     int safePos(int n, int k) {
-        // code here
-        vector<int> v;
-        for(int i=0;i<n;i++)
-        v.push_back(i+1);
-        solve(v,k-1,0);
-        return v[0];
+        if(n==1)
+        return 1;
+       int x=safePos(n-1,k); 
+       return (x+k-1)%n+1;
     }
 };
 
