fad1613168e35be3ce5caa36dcecea25af8c8f66
sargun06 <55631683+sargun06@users.noreply.github.com>
2022-08-14 18:12:57 +0530

    Time: 18 ms (83.27%), Space: 11.6 MB (29.96%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -10,31 +10,29 @@
  */
 class Solution {
 public:
-    void reverse(ListNode* s, ListNode *e)
+    void reverse( ListNode* s, ListNode* e)
     {
-        ListNode* p= NULL, *c=s, *n=s->next;
+        ListNode* p= NULL, * c=s, * n=s->next;
         while(p!=e)
-        {
+        {  
             c->next=p;
             p=c;
             c=n;
-            if(n!=NULL)
-                n=n->next;
+            if(n!=NULL) n=n->next;
         }
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL || k==1)
-            return head;
-        int z=k-1;
-        ListNode *s=head, *e=head;
-        while(z--)
+        if(head==NULL || head->next==NULL || k==1) return head;
+        ListNode* s=head, * e= head;
+        int inc=k-1;
+        while(inc--)
         {
             e=e->next;
-            if(e==NULL) return s;
+            if(e==NULL) return head;
         }
-        ListNode* new_head=reverseKGroup(e->next,k);
+        ListNode* newhead= reverseKGroup(e->next, k);
         reverse(s,e);
-        s->next=new_head;
+        s->next=newhead;
         return e;
     }
 };
